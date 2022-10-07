ec7c00e27aaa73ed2e6096c24e866bac5dc34ec3
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2022-10-07 22:28:02 +0530

    Sidebar error resolved

--- client/src/Components/sidebar.js
+++ client/src/Components/sidebar.js
@@ -2,31 +2,34 @@ import React from "react";
 import './sidebar.css';
 import Internship from "../Pages/Internship";
 import Image from '../radicalx.png';
-import {TbLayoutDashboard}from 'react-icons/tb';
-import{HiBadgeCheck,HiUserCircle} from 'react-icons/hi';
-import{MdOutlineMenuBook, MdWorkOutline} from 'react-icons/md';
-import {FiSettings} from 'react-icons/fi';
-import {BrowserRouter, Router} from 'react-router-dom';
-function Sidebar(){
-    return(
-        <div class="space">
-     <div class="image">
-       <img src={Image} alt="Logo" />
-     </div>
-     <div class="rows"><TbLayoutDashboard/><span> Dashboard</span> </div>
-     <div class="rows"> <HiBadgeCheck/><span> Apprenticeship</span></div>
-     <div class="rows  "><MdOutlineMenuBook/>
-     <span>
-     <BrowserRouter>
-        
-       <a href={<Internship/>} target="_self">Internship</a>
-       
-       </BrowserRouter></span>
-       </div>
-     <div class="rows"><MdWorkOutline/><span> Jobs</span></div>
-     <div class="rows"> <FiSettings/><span>Settings</span></div>
-     <div className="xsa"> <HiUserCircle/><span>User</span>s</div>
-     </div>
-    )
+import { TbLayoutDashboard } from 'react-icons/tb';
+import { HiBadgeCheck, HiUserCircle } from 'react-icons/hi';
+import { MdOutlineMenuBook, MdWorkOutline } from 'react-icons/md';
+import { FiSettings } from 'react-icons/fi';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Sidebar() {
+  return (
+    <div className="space">
+      <div className="image">
+        <img src={Image} alt="Logo" />
+      </div>
+      <div className="rows"><TbLayoutDashboard /><span> Dashboard</span> </div>
+      <div className="rows"> <HiBadgeCheck /><span> Apprenticeship</span></div>
+      <div className="rows  "><MdOutlineMenuBook />
+        <span>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/internship" element={<Internship />} />
+            </Routes>
+          </BrowserRouter>
+        </span>
+        Internship
+      </div>
+      <div className="rows"><MdWorkOutline /><span> Jobs</span></div>
+      <div className="rows"> <FiSettings /><span>Settings</span></div>
+      <div className="xsa"> <HiUserCircle /><span>User</span>s</div>
+    </div>
+  )
 }
 export default Sidebar;
